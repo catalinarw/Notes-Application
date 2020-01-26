cb78d1f9e2093c118f4cc87caac650882bddaa1b
Catalina Williams <catalina.rose1361@gmail.com>
2020-01-26 16:36:43 -0700

    index.html connected to the server

--- assets/js/server.js
+++ assets/js/server.js
@@ -8,7 +8,17 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// savedNotes = ["apple", "pear"];
+
+// app.get('/', function (req, res) {
+//     res.send(index.html)
+// })
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../../index.html"));
+  });
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-  });
+});
